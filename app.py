6c050bfb734ae80ba593e3f8784a7de25fbe6218
Melanie <melanie@dhcp-10-105-150-236.wireless.northwestern.private>
2016-04-04 20:11:39 -0500

    Moving static files and serving index

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # app.py
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/hello')
 def hello():
 	return 'Hello world!'
+
+@app.route('/')
+def root():
+	return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
