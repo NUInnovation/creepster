24e166d74283f6211134217cd2d95217f98df646
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-04-06 11:18:09 -0500

    adding function into search_twitter

--- search_twitter.py
+++ search_twitter.py
@@ -4,20 +4,34 @@ from twitter import *
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
 def search_user_twitter(query):
-	config = {}
-
 	t = Twitter(
 		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
 	)
 
 	results = t.users.search(q = query)
-	#
+
 	for user in results:
 		print "@%s (%s): %s" % (user["screen_name"], user["name"], user["location"])
 
+
+def search_tweets_for_user(screen_name, count):
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+
+	results = t.statuses.user_timeline(screen_name=screen_name, count=count)
+
+	print len(results)
+	for tweet in results:
+		print '----'
+		print tweet
+
+
 def main():
-	user_name = 'Melanie Klerer'
-	search_user_twitter(user_name)
+	# user_name = 'Erin Funk'
+	# search_user_twitter(user_name)
+	screen_name = 'dys_FUNKtional'
+	search_tweets_for_user(screen_name, 50)
 
 
 if __name__ == '__main__':
