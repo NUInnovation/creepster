0a923894280334b97e4bfa65561ca4794a2a7a38
Nevil George <nevil@u.northwestern.edu>
2016-04-06 11:18:55 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Credentails for Twitter Application
-# Using Melanie's Twitter for Access tokens
-
-consumer_key = 'pXjvkiLDVwrsERC3FJDQ87clP'
-consumer_secret = 'roJFLGlrPqjzEA8MVkisktW5vV11moFvosbHYfxK8KXyD0blCJ'
-access_token = '2787350504-0KBvCMlzd3PxkkbCSekrJaOO79HkdYUNesWTKiV'
-access_token_secret = 'OqIEDuRTxo5cXUQxFNPH1jok8EBDQ5ePPJ7pgmPK4DWcR'
