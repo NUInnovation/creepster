f149b466a63278c663cf89366e49d8b8dc920d85
Melanie <melanie@Melanies-MacBook-Pro.local>
2016-04-06 20:24:43 -0500

    Added some useful functionality, like hashtag aggregator for twitter. Unsure of how to use yet.

--- search_twitter.py
+++ search_twitter.py
@@ -3,15 +3,14 @@
 from twitter import *
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
-def search_user_twitter(query):
+def search_username(query):
 	t = Twitter(
 		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
 	)
 
 	results = t.users.search(q = query)
-
-	for user in results:
-		print "@%s (%s): %s" % (user["screen_name"], user["name"], user["location"])
+	user = results[0]
+	return user["screen_name"]
 
 
 def search_tweets_for_user(screen_name, count):
@@ -21,17 +20,75 @@ def search_tweets_for_user(screen_name, count):
 
 	results = t.statuses.user_timeline(screen_name=screen_name, count=count)
 
-	print len(results)
 	for tweet in results:
 		print '----'
-		print tweet
+		print tweet["text"]
+
+def is_geo_enabled(screen_name):
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+	results = t.users.search(q=screen_name, count=1)
+	return results[0]["geo_enabled"]
+
+def user_location(screen_name):
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+
+	results = t.users.search(q=screen_name, count=1)
+	return results[0]["location"]
+
+def user_description(screen_name):
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+
+	results = t.users.search(q=screen_name, count=1)
+	return results[0]["description"]
+
+def search_geo_for_user(screen_name, count):
+	if is_geo_enabled(screen_name) == False:
+		print "Not Geo-Enabled"
+		return
+
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+
+	results = t.statuses.user_timeline(screen_name=screen_name, count=count)
+
+	for tweet in results:
+		if tweet["place"] != None:
+			print tweet["place"]
+
+def aggregate_hashtags(screen_name, count):
+
+	t = Twitter(
+		auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
+	)
+
+	results = t.statuses.user_timeline(screen_name=screen_name, count=count)
+	hashtag_map = {}
+	for tweet in results:
+		for hashtag in tweet["entities"]["hashtags"]:
+			name = hashtag["text"]	
+			if not name in hashtag_map:
+				hashtag_map[name] = 1
+			else:
+				hashtag_map[name] = hashtag_map[name] + 1
+
+	sort_hash = sorted(hashtag_map, key=hashtag_map.get, reverse=True)
+	return sort_hash[:4]
 
 
 def main():
-	# user_name = 'Erin Funk'
-	# search_user_twitter(user_name)
-	screen_name = 'dys_FUNKtional'
-	search_tweets_for_user(screen_name, 50)
+	user_name = 'Erin Funk'
+	sn = search_username(user_name)
+	search_tweets_for_user(sn, 50)
+	print user_location(sn)
+	print user_description(sn)
+	print aggregate_hashtags(sn, 1000)
 
 
 if __name__ == '__main__':
