d1fe22f1d7b1bdd5b8aea168c01905a2fa3bdca6
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-04-13 20:03:12 -0500

    parses out locations from instagram pictures

--- instagram_client.py
+++ instagram_client.py
@@ -7,6 +7,8 @@ class InstagramClient:
 
     def __init__(self):
         self.api_url = 'https://api.instagram.com/v1/'
+        self.user_profile = None
+        self.user_media = None
 
 
     def find_usernames(self, name):
@@ -23,16 +25,36 @@ class InstagramClient:
 
     def get_user_profile(self, user_id):
         """Get more information about a user given a user_id."""
-        url = self.api_url + 'users/' + user_id + '/?'
-        params = {'access_token': instagram['access_token']}
-        response = requests.get(url + 'users/' + user_id, params=params)
+        if not self.user_profile:
+            url = self.api_url + 'users/' + user_id + '/?'
+            params = {'access_token': instagram['access_token']}
+            response = requests.get(url + 'users/' + user_id, params=params)
+            self.user_profile = response.json()
 
-        return response.json()
+        return self.user_profile
 
 
     def get_user_media(self, username):
         """Get all media given a username."""
-        url = 'https://instagram.com/' + username + '/media'
-        response = requests.get(url)
+        if not self.user_media:
+            url = 'https://instagram.com/' + username + '/media'
+            response = requests.get(url)
+            self.user_media = response.json()
 
-        return response.json()
+        return self.user_media
+
+    def get_location_names(self, username):
+        """Get all locations from Instagram posts of the given username."""
+        if not self.user_media:
+            self.get_user_media(username)
+
+        # parse media and grab location names
+        items = self.user_media['items']
+        location_names = []
+        for item in items:
+            location = item['location']
+            if location:
+                location_name = location['name']
+                location_names.append(location_name)
+
+        return location_names
--- search_instagram.py
+++ search_instagram.py
@@ -3,10 +3,10 @@ from instagram_client import InstagramClient
 
 def main():
     insta = InstagramClient()
-    usernames = insta.find_usernames('Miley Cyrus')
-    media = insta.get_user_media(usernames[0])
+    usernames = insta.find_usernames('Melanie Klerer')
+    locations = insta.get_location_names(usernames[0])
 
-    print media
+    print locations
 
 if __name__ == '__main__':
     main()
