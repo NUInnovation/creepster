069f272356cb3f0a71b154c585c44fe6f035e3bf
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-05-01 17:14:49 -0500

    loader add

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
-  $("#search-box").keypress(function(event) {
+$(document).ready(function() {
+  $('#search-box').keypress(function(event) {
       if (event.which == 13) {
         event.preventDefault();
         if ($('#search-box').val() !== '') {
-          $("form").submit();
+          $('form').submit();
+          // show loader
+          $('#home-content').css("display", "none");
+          $('#loader').css("display", "block");
         }
       }
   });
