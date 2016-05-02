513e97ceda87c283bf2cb294c472f953d381e0ac
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-05-01 19:46:55 -0500

    changing error to warning, code will run with no config.json

--- run.py
+++ run.py
@@ -9,14 +9,12 @@ def set_environment_vars():
     If the file is not found, an error message is printed."""
     try:
         config_file = open('config.json', 'r')
+        config_json = json.load(config_file)
+        for key, value in config_json.iteritems():
+            os.environ[key] = value
     except IOError:
         # catch missing file or other IOError
-        print 'ERROR: config.json not found in root directory!'
-        sys.exit()
-
-    config_json = json.load(config_file)
-    for key, value in config_json.iteritems():
-        os.environ[key] = value
+        print 'WARNING: config.json not found in root directory!'
 
 
 from app import app
