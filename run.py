3b9ecfc4def611fa25a59ab04697e7e4400267f5
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-05-15 18:55:43 -0500

    creates cache folder if it does not already exist

--- run.py
+++ run.py
@@ -4,6 +4,15 @@ import json
 import os
 import sys
 
+
+def create_cache_folder():
+    """Creates a folder in the 'app' directory to hold cached files if it
+    does not already exist."""
+    cache_directory = 'app/cache'
+    if not os.path.exists(cache_directory):
+        os.makedirs(cache_directory)
+
+
 def set_environment_vars():
     """Sets environment variables from a config.json file in the root directory.
     If the file is not found, a warning message is printed."""
@@ -18,6 +27,8 @@ def set_environment_vars():
 
 
 from app import app
-set_environment_vars()
+
 if __name__ == '__main__':
+    set_environment_vars()
+    create_cache_folder()
     app.run(debug=True)
