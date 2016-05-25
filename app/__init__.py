48082598838d2b5071918dd44766bb56d833a41f
Nevil George <nevilgeorge2016@u.northwestern.edu>
2016-05-24 19:36:15 -0500

    adding explicit logger for Heroku debugging

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 # __init__.py
 from flask import Flask
+import logging
+import sys
 
 app = Flask(__name__, static_url_path='/static')
 # app.config.from_object('config')
+
+# logging for Heroku
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from app import views
